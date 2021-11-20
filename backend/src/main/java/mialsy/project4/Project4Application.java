c2ac4a0aa7ea8322edbf9521aa18ef92e6bffa38
Chuxi Wang <wangchuxi94@gmail.com>
2021-11-19 22:41:54 -0800

    updated gitignore, add user controller

--- backend/src/main/java/mialsy/project4/Project4Application.java
+++ backend/src/main/java/mialsy/project4/Project4Application.java
@@ -8,10 +8,22 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
+@RestController
 public class Project4Application {
-//	private static Session session;
+
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project4Application.class, args);
---
+++ backend/src/main/java/mialsy/project4/controllers/UserController.java
@@ -0,0 +1,7 @@
+package mialsy.project4.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
