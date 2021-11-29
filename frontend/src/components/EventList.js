53d04628965e36fd0d51a67d4fdcda18268ec8f7
Chuxi Wang <wangchuxi94@gmail.com>
2021-11-29 01:24:23 -0800

    install dependency again, and removed mock data

--- frontend/src/components/EventList.js
+++ frontend/src/components/EventList.js
@@ -4,14 +4,6 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { BASE_URL } from '../constants/Constant';
 
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
-
 const EventList = () => {
     const [events, setEvents] = useState([]);
     const opt = {
