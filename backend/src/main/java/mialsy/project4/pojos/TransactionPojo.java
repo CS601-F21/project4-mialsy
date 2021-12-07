b941b69957d449dc6da06e7cacbd5e1299179e87
Chuxi Wang <wangchuxi94@gmail.com>
2021-12-06 22:52:10 -0800

    add image upload support, add location

--- backend/src/main/java/mialsy/project4/pojos/TransactionPojo.java
+++ backend/src/main/java/mialsy/project4/pojos/TransactionPojo.java
@@ -14,13 +14,16 @@ public class TransactionPojo {
 
     private final Long eventTime;
 
-    public TransactionPojo(Long id, Long userId, Long eventId, String eventName, String eventDescription, Long eventTime) {
+    private final String eventLocation;
+
+    public TransactionPojo(Long id, Long userId, Long eventId, String eventName, String eventDescription, Long eventTime, String eventLocation) {
         this.id = id;
         this.userId = userId;
         this.eventId = eventId;
         this.eventName = eventName;
         this.eventDescription = eventDescription;
         this.eventTime = eventTime;
+        this.eventLocation = eventLocation;
     }
 
     public Long getId() {
@@ -45,5 +48,9 @@ public class TransactionPojo {
 
     public Long getEventTime() {
         return eventTime;
+    }
+
+    public String getEventLocation() {
+        return eventLocation;
     }
 }
